ca1239d58c29192f4d533c0e18f395d38ec5aca5
alfinfebrian05 <ferdinandusalfin05@gmail.com>
2026-03-27 01:10:31 +0700

    feat: add vercel/speed-insights

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Theme>
           {children}
+          <SpeedInsights />
         </Theme>
       </body>
     </html>
